126f7aeed1b570c8ec24c7361129c42cdf6a798d
yosef32 <yosef32@gmail.com>
2022-01-02 23:00:19 +0200

    project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Model } from './model';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fetchURL';
+
+  model: Model = {
+    url: "",
+    res: ""
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  async fetch() {
+    this.model.res = await (await axios.get(this.model.url)).data
+  }
 }
---
+++ src/app/model.ts
@@ -0,0 +1,4 @@
+export interface Model {
+    url: string;
+    res: string;
+}
